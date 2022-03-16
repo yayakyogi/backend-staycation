0c510474c767a1e1c565a2dd68e9d10a17a7b3dc
yayakyogi <yayaktaka@gmail.com>
2022-03-16 22:57:37 +0700

    deploy heroku

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -2,7 +2,8 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: "public/images",
+  // destination: "public/images",
+  destination: "public/images/online",
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
   },
--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/staycation", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  // mongodb://127.0.0.1:27017/staycation
+  "mongodb+srv://yayakyogi:GHyMnbiwiIUqDSUN@cluster0.zqxyq.mongodb.net/staycation?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
