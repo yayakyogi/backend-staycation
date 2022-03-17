7635de502c480a72e8b85f23dfbd1dd32b10ac22
yayakyogi <yayaktaka@gmail.com>
2022-03-17 23:56:05 +0700

    try heroku

--- router/index.js
+++ router/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 // GET home page
 router.get("/", (req, res, next) => {
-  res.redirect("/admin/signin");
+  // res.redirect("/admin/signin");
+  res.send("Hello /");
 });
 
 module.exports = router;
