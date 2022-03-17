c668543dc8ff750883d013d1157944638e772bbe
yayakyogi <yayaktaka@gmail.com>
2022-03-17 13:11:12 +0700

    try deploy heroku

--- app.js
+++ app.js
@@ -46,6 +46,11 @@ app.get("/", (req, res) => {
   res.send("Hellow World!");
 });
 
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
 // handle page not found
 // app.get("*", (req, res) => {
 //   res.status(404);
