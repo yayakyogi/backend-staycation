e2a13c2cde1acd657f4d99fb0b3b3310e4af97ac
yayakyogi <yayaktaka@gmail.com>
2022-03-18 00:29:31 +0700

    try heroku

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  // "mongodb://127.0.0.1:27017/staycation",
+  // "mongodb://127.0.0.1:27017/staycation",//
   "mongodb+srv://yayakyogi:GHyMnbiwiIUqDSUN@cluster0.zqxyq.mongodb.net/staycation?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
