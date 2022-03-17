d331dd6c61c140b66431cb5cff55bf9a2239f5d5
yayakyogi <yayaktaka@gmail.com>
2022-03-17 12:35:10 +0700

    try route app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const app = express();
-const PORT = process.env.PORT || "8080";
+const PORT = process.env.PORT || 5000;
 
 // require route
 const indexRouter = require("./router/index");
