eec7cfb2dc1d420593c360f39c08dd5c82ce5c2e
yayakyogi <yayaktaka@gmail.com>
2022-03-18 20:25:56 +0700

    fix change isPopular on landing-page API

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  // "mongodb://127.0.0.1:27017/staycation",//
-  "mongodb+srv://yayakyogi:GHyMnbiwiIUqDSUN@cluster0.zqxyq.mongodb.net/staycation?retryWrites=true&w=majority",
+  "mongodb://127.0.0.1:27017/staycation",
+  // "mongodb+srv://yayakyogi:GHyMnbiwiIUqDSUN@cluster0.zqxyq.mongodb.net/staycation?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
